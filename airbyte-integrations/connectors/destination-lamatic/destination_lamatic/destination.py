ce5d6b13b92aede6ac31a437e7509839308db2dd
Vansh Arora <vansh.vansharora2003@gmail.com>
2024-04-08 12:32:33 +0530

    reconnection to rabbitmq host added

--- airbyte-integrations/connectors/destination-lamatic/destination_lamatic/destination.py
+++ airbyte-integrations/connectors/destination-lamatic/destination_lamatic/destination.py
@@ -88,29 +88,50 @@ def map_data(data_mapping, body, required_fields):
 
 def consume_messages(config):
     # Establish a new connection and channel for each thread
-    host =  config.get('host')
+    max_retries = 3
+
+    def reconnect(connection):
+        if connection:
+            if connection.is_open:
+                connection.close()
+
+        connection = create_connection(config=config)
+        return connection, connection.channel()
+    
+    connection, channel = reconnect(None)
+
     pod_URL = config.get('pod_URL')
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-    channel = connection.channel()
     data_mapping = config.get("data_mapping","")
     bearer_token = config.get("bearer_token", "")
     required_fields = config.get("required_fields", "")
     
     # Ensure the queue exists
     # channel.queue_declare(queue=queue_name, durable=True)
-    result = channel.queue_declare(queue='', exclusive=True)
-    queue_name = result.method.queue
 
-    channel.queue_bind(
-        exchange=_EXCHANGE, queue=queue_name, routing_key=_ROUTING_KEY)
-    
-    # Set up a consumer
-    for method_frame, properties, body in channel.consume(queue=queue_name, auto_ack=False, inactivity_timeout=60):
-        if method_frame:
-            print(f" [x] Received {body.decode()}")
+    for attempt in range(max_retries):
+        try: 
+            result = channel.queue_declare(queue='', exclusive=True)
+            queue_name = result.method.queue
 
-            if (data_mapping): 
-                try:
+            channel.queue_bind(
+                exchange=_EXCHANGE, queue=queue_name, routing_key=_ROUTING_KEY)
+            
+        except pika.exceptions.StreamLostError as e:
+            print(f"StreamLostError detected, attempting to reconnect, attempt {attempt + 1}")
+            connection, channel = reconnect(connection)
+            continue  # Continue to the next attempt
+
+        except Exception as e:
+            print(f"Unexpected error: {e}, attempting to reconnect, attempt {attempt + 1}")
+            connection, channel = reconnect(connection)
+            continue  # Continue to the next attempt
+            
+            # Set up a consumer
+        for method_frame, properties, body in channel.consume(queue=queue_name, auto_ack=False, inactivity_timeout=60):
+            if method_frame:
+                print(f" [x] Received {body.decode()}")
+
+                if (data_mapping): 
                     mapped_data = map_data(data_mapping, json.loads(body.decode()), required_fields)
                     print(f"Mapped Data: {mapped_data}")
 
@@ -129,27 +150,28 @@ def consume_messages(config):
                     print(pod_response)
                     print(pod_response.text)
                     print(" [x] Sent the Data to Pod")
-                except Exception as e:
-                    print("Exception occured in sending response to API", e)
-            
+                
+                else:
+                    print("No data mapping is provided")
+                    # Acknowledge the message
+                channel.basic_ack(delivery_tag=method_frame.delivery_tag)
             else:
-                print("No data mapping is provided")
-
-            # Acknowledge the message
-            channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-        else:
-            # Inactivity timeout reached, check if the thread should stop
-            print("Stopping the Message Consumer for this Invocation of Write function")
-            # Implement your logic here to decide whether to break the loop
-            # For example, you can check a condition or wait for a signal
-            break
+                # Inactivity timeout reached, check if the thread should stop
+                print("Stopping the Message Consumer for this Invocation of Write function")
+                # Implement your logic here to decide whether to break the loop
+                # For example, you can check a condition or wait for a signal
+                break
+        break
     
     # Cancel the consumer and close the connection when done
     channel.cancel()
-    connection.close()
+
+    if connection.is_open:
+        connection.close()
 
 
 def start_consumer_thread(config):
+    print("Consumer Thread Starting")
     consumer_thread = threading.Thread(target=consume_messages, args=(config,))
     consumer_thread.start()
     return consumer_thread
@@ -176,51 +198,84 @@ class DestinationLamatic(Destination):
         :return: Iterable of AirbyteStateMessages wrapped in AirbyteMessage structs
         """
         print("Executing Write function")
+
+        max_retries = 3  # Max reconnection attempts
+
         consumer_thread = start_consumer_thread(config)
         time.sleep(1)
 
-        connection = create_connection(config=config)
-        channel = connection.channel()
+        def attempt_publish(connection, channel, message):
+            try:
+                headers = {"stream": message.record.stream, "emitted_at": message.record.emitted_at, "namespace": message.record.namespace}
+                properties = BasicProperties(content_type="application/json", headers=headers)
+                channel.basic_publish(
+                    exchange=_EXCHANGE, routing_key=_ROUTING_KEY, properties=properties, body=json.dumps(message.record.data)
+                )
+                return True
+            except pika.exceptions.AMQPError as e:
+                print(f"Error publishing message: {e}")
+                return False
+
+        def reconnect(connection):
+            if connection:
+                if connection.is_open:
+                    connection.close()
+
+            connection = create_connection(config=config)
+            return connection, connection.channel()
+        
+        connection, channel = reconnect(None)  # Initial connection setup
 
         streams = {s.stream.name for s in configured_catalog.streams}
-        try:
-            for message in input_messages:
-                if message.type == Type.STATE:
-                    # Emitting a state message means all records that came before it
-                    # have already been published.
-                    yield message
-                elif message.type == Type.RECORD:
-                    record = message.record
-                    if record.stream not in streams:
-                        # Message contains record from a stream that is not in the catalog. Skip it!
-                        continue
-                    headers = {"stream": record.stream, "emitted_at": record.emitted_at, "namespace": record.namespace}
-                    properties = BasicProperties(content_type="application/json", headers=headers)
-                    channel.basic_publish(
-                        exchange=_EXCHANGE or "", routing_key=_ROUTING_KEY, properties=properties, body=json.dumps(record.data)
-                    )
-                else:
-                    # Let's ignore other message types for now
+        for message in input_messages:
+            if message.type == Type.STATE:
+                yield message
+            elif message.type == Type.RECORD:
+                record = message.record
+                if record.stream not in streams:
+                    # Message contains record from a stream that is not in the catalog. Skip it!
                     continue
-            consumer_thread.join()
-        finally:
+
+                success = attempt_publish(connection, channel, message)
+
+                if not success:
+                    print("Entering not success")
+                    for attempt in range(max_retries):
+                        print(f"Attempting to reconnect and retry, attempt {attempt + 1}")
+
+                        consumer_thread.join()
+                        consumer_thread = start_consumer_thread(config)
+                        time.sleep(1)
+
+                        connection, channel = reconnect(connection)
+                        success = attempt_publish(connection, channel, message)
+                        if success:
+                            break
+                        else:  # If all retries failed
+                            print("Failed to publish message after multiple reconnection attempts.")
+            else:
+                # Let's ignore other message types for now
+                continue
+
+        consumer_thread.join()
+
+        if connection.is_open:
             connection.close()
 
+
     def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
-        """
-        Tests if the input configuration can be used to successfully connect to the destination with the needed permissions
-            e.g: if a provided API token or password can be used to connect and write to the destination.
-
-        :param logger: Logging object to display debug/info/error to the logs
-            (logs will not be accessible via airbyte UI if they are not passed to this logger)
-        :param config: Json object containing the configuration of this destination, content of this json is as specified in
-        the properties of the spec.json file
-
-        :return: AirbyteConnectionStatus indicating a Success or Failure
-        """
         try:
-            # TODO
-
-            return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+            connection = create_connection(config=config)
         except Exception as e:
+            logger.error(f"Failed to create connection. Error: {e}")
+            return AirbyteConnectionStatus(status=Status.FAILED, message=f"Could not create connection: {repr(e)}")
+        try:
+            channel = connection.channel()
+            if channel.is_open:
+                return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+            return AirbyteConnectionStatus(status=Status.FAILED, message="Could not open channel")
+        except Exception as e:
+            logger.error(f"Failed to open RabbitMQ channel. Error: {e}")
             return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
+        finally:
+            connection.close()
